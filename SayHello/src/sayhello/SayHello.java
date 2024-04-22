1a585c37a37a80fbea7dad1ef1fa1733281e252c
I Made Dharma Putra <57142530+dhrmputra@users.noreply.github.com>
2024-04-22 08:15:27 +0800

    Add files via upload

--- SayHello/src/sayhello/SayHello.java
+++ SayHello/src/sayhello/SayHello.java
@@ -4,6 +4,9 @@
  * Project : Hello World
  */
 package sayhello;
+
+import java.util.Scanner;
+
 /**
  *
  * @author INSTIKI
@@ -13,9 +16,26 @@ public class SayHello {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hallo Dharma apa kabar?");
-        System.out.println("Anda tinggal di denpasar");
-        System.out.println("Selamat belajar Java");
+        String b1,b2;
+        int b3,rumus;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukan Nama Anda : ");
+        b1 = sc.nextLine();
+        
+        System.out.print("Masukan NIM Anda : ");
+        b2 = sc.nextLine();
+        
+        System.out.print("Masukan Tahun Lahir Anda : ");
+        b3 = sc.nextInt();
+        
+        rumus = 2024 - b3;
+               
+        System.out.println("Nama Saya " + b1);
+        System.out.println("Dengan NIM " + b2);
+        System.out.println("Tahun Kelahiran " + b3);
+        System.out.println("Umur anda adalah : " + rumus );
         
         
     }
